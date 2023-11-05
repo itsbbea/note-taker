58e7953911862d6badac62a8ffb88d05f517f33b
itsbbea <bea.may27@hotmail.com>
2023-11-06 00:47:33 +1100

    defined API rotues to get, save and delete notes, created html.js to create path for html pages

--- Develop/assets/server.js
+++ Develop/assets/server.js
@@ -11,3 +11,16 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/api/notes', (req, res) => {
+    // Implement the code to retrieve notes from a database or JSON file
+  });
+  
+  app.post('/api/notes', (req, res) => {
+    // Implement the code to save a new note
+  });
+  
+  app.delete('/api/notes/:id', (req, res) => {
+    // Implement the code to delete a note
+  });
+  
